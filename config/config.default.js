f033aa7b515275917bdcd2ebed585c51d9d21ccb
NiRongFei <fei332085762@aliyun.com>
2019-08-12 18:49:37 +0800

    提交部署

---
+++ config/config.default.js
@@ -0,0 +1,46 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1565602075298_3034';
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+  config.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
+
+  // add your middleware config here
+  config.middleware = [ 'robot' ];
+
+  config.robot = {
+    ua: [ /curl/i, /Baiduspider/i ],
+  };
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
